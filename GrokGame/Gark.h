5df8fae070ea6efdaa8cb92787edbb259937ba64
jasperlin0330 <52628141+jasperlin0330@users.noreply.github.com>
2022-10-03 08:46:00 -0700

    Add files via upload

---
+++ GrokGame/Gark.h
@@ -0,0 +1,29 @@
+// Gark.h
+
+#ifndef GARK_INCLUDED
+#define GARK_INCLUDED
+
+class Mesa;
+
+class Gark
+{
+  public:
+        // Constructor
+    Gark(Mesa* mp, int r, int c);
+
+        // Accessors
+    int  row() const;
+    int  col() const;
+
+        // Mutators
+    void move();
+    bool getAttacked(int dir);
+
+  private:
+    Mesa* m_mesa;
+    int   m_row;
+    int   m_col;
+    int   m_health;
+};
+
+#endif  // GARK_INCLUDED
